576a386a86831662bebce325d79a6dea3cd98c5f
Nick Prühs <dev@npruehs.de>
2019-12-15 22:53:18 +0100

    Add main and mission fragment and provide additional layout for landscape orientation.

--- src/app/src/main/java/de/npruehs/missionrunner/MainActivity.java
+++ src/app/src/main/java/de/npruehs/missionrunner/MainActivity.java
@@ -1,21 +1,40 @@
 package de.npruehs.missionrunner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements MainFragment.OnMainFragmentInteractionListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void showMissions(View view) {
-        Intent intent = new Intent(this, ShowMissionsActivity.class);
-        startActivity(intent);
+    @Override
+    public void onShowMissions() {
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            // Check what fragment is currently shown, replace if needed.
+            ShowMissionsFragment showMissionsFragment = (ShowMissionsFragment)
+                    getSupportFragmentManager().findFragmentById(R.id.frameLayoutDetails);
+            if (showMissionsFragment == null) {
+                // Make new fragment to show this selection.
+                showMissionsFragment = ShowMissionsFragment.newInstance();
+
+                // Execute a transaction, replacing any existing fragment
+                // with this one inside the frame.
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.frameLayoutDetails, showMissionsFragment);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.commit();
+            }
+        } else {
+            Intent intent = new Intent(this, ShowMissionsActivity.class);
+            startActivity(intent);
+        }
     }
 }
---
+++ src/app/src/main/java/de/npruehs/missionrunner/ShowMissionsFragment.java
@@ -0,0 +1,47 @@
+package de.npruehs.missionrunner;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ShowMissionsFragment extends Fragment {
+    public ShowMissionsFragment() {
+        // Required empty public constructor
+    }
+
+    public static ShowMissionsFragment newInstance() {
+        ShowMissionsFragment f = new ShowMissionsFragment();
+        return f;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_show_missions, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    public interface OnShowMissionsFragmentInteractionListener {
+    }
+}
