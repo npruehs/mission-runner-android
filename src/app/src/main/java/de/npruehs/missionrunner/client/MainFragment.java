5a3ce67efb406f847993eaafd7333e99943ad312
Nick Prühs <dev@npruehs.de>
2020-01-01 03:35:51 +0100

    Use options menu instead of plain buttons.

--- src/app/src/main/java/de/npruehs/missionrunner/client/MainFragment.java
+++ src/app/src/main/java/de/npruehs/missionrunner/client/MainFragment.java
@@ -1,16 +1,17 @@
 package de.npruehs.missionrunner.client;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -23,9 +24,8 @@ import javax.inject.Inject;
 import de.npruehs.missionrunner.client.model.Account;
 import de.npruehs.missionrunner.client.model.AccountViewModel;
 import de.npruehs.missionrunner.client.model.Resource;
-import de.npruehs.missionrunner.client.model.ResourceStatus;
 
-public class MainFragment extends Fragment implements View.OnClickListener, Observer<Resource<Account>> {
+public class MainFragment extends Fragment implements Observer<Resource<Account>> {
     @Inject
     AccountViewModel viewModel;
 
@@ -43,6 +43,8 @@ public class MainFragment extends Fragment implements View.OnClickListener, Obse
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -55,10 +57,6 @@ public class MainFragment extends Fragment implements View.OnClickListener, Obse
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        // Register for button events.
-        Button b = view.findViewById(R.id.buttonMissions);
-        b.setOnClickListener(this);
 
         // Bind view to view model.
         textViewAccountName = view.findViewById(R.id.textViewAccountName);
@@ -91,12 +89,21 @@ public class MainFragment extends Fragment implements View.OnClickListener, Obse
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.buttonMissions:
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.menu_main, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_missions:
                 showMissions();
                 break;
         }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
